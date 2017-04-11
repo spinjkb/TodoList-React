673ab8abb6eec8b2b03c3259e049aae277500cd6
spinjkb <960713393@qq.com>
2017-04-12 02:39:58 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoInput from './TodoInput';
+import TodoItem from './TodoItem';
+import 'normalize.css';
+import './reset.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newTodo: '',
+      todoList: [
+        { id: 1, title: '第一个待办' },
+        { id: 2, title: '第二个待办' },
+        { id: 3, title: '第三个待办' },
+      ]
+    }
+  }
+
+
   render() {
+    let todos = this.state.todoList.map((item, index) => {
+      return (
+        <li>
+          <TodoItem todo={item} />
+        </li>
+      )
+
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1>我的待办</h1>
+        <div className="inputWrapper">
+          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ol>
+          {todos}
+        </ol>
       </div>
     );
+  }
+  addTodo() {
+    console.log('我得添加一个 todo 了')
   }
 }
 
