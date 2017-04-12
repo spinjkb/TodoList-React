eba4d4f9d779d3ac9db6700aa3de90a3be33f368
spinjkb <960713393@qq.com>
2017-04-12 10:08:15 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -17,22 +17,23 @@ class App extends Component {
       ]
     }
   }
-
-
   render() {
     let todos = this.state.todoList.map((item, index) => {
       return (
-        <li>
-          <TodoItem todo={item} />
+        <li key={index}>
+          <TodoItem todo={item} 
+          onToggle={this.toggle.bind(this)}
+          onDelete={this.delete.bind(this)}/>
         </li>
       )
-
     })
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo} />
+          <TodoInput content={this.state.newTodo} 
+          onChange={this.changeTitle.bind(this)}
+          onSubmit={this.addTodo.bind(this)}/>
         </div>
         <ol>
           {todos}
@@ -40,9 +41,39 @@ class App extends Component {
       </div>
     );
   }
-  addTodo() {
-    console.log('我得添加一个 todo 了')
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList:this.state.todoList
+    })
+  }
+  toggle(e,todo){
+    todo.status = todo.status ==='completed' ? '' : 'completed';
+    this.setState(this.state) 
+  }
+  delete(event,todo){
+    todo.deleted = true;
+    this.setState(this.state) 
+  }
+  addTodo(event) {
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList 
+    })
+    console.log(this.state.todoList)
   }
 }
 
 export default App;
+
+let id = 0
+function idMaker() {
+  id += 1
+  return id
+}
