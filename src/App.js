222a0edf22056708077f669490fcc562ae58b022
spinjkb <960713393@qq.com>
2017-04-19 13:43:50 +0800

    componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -38,24 +38,23 @@ class App extends Component {
       </div>
     );
   }
-  
+  componentDidUpdate(){
+    localStore.save('todoList', this.state.todoList)
+  }
 
   changeTitle(event) {
     this.setState({
       newTodo: event.target.value,
       todoList: this.state.todoList
     })
-    localStore.save('todoList', this.state.todoList)
   }
   toggle(e, todo) {
     todo.status = todo.status === 'completed' ? '' : 'completed';
     this.setState(this.state)
-    localStore.save('todoList',this.state.todoList)
   }
   delete(event, todo) {
     todo.deleted = true;
     this.setState(this.state)
-    localStore.save('todoList', this.state.todoList)
   }
   addTodo(event) {
     if (event.target.value === '') {
@@ -71,7 +70,6 @@ class App extends Component {
         newTodo: '',
         todoList: this.state.todoList
       })
-      localStore.save('todoList', this.state.todoList)
     }
 
   }
