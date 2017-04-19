6b52c7c4251b9b80e1ab6f47c60aea937a59a854
spinjkb <960713393@qq.com>
2017-04-20 00:10:29 +0800

    消除onchange警告

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      user: {},
       newTodo: '',
       // todoList: localStore.load('todoList') || []
       todoList: []
@@ -45,7 +46,7 @@ class App extends Component {
     })
     return (
       <div className="App">
-        <h1>我的待办</h1>
+        <h1>{this.state.user.username || '我'}的待办</h1>
         <div className="inputWrapper">
           <TodoInput content={this.state.newTodo}
             onChange={this.changeTitle.bind(this)}
@@ -54,9 +55,14 @@ class App extends Component {
         <ol className="todoList">
           {todos}
         </ol>
-        <Login/>
+        <Login onSignUp={this.onSignUp.bind(this)} />
       </div>
     );
+  }
+  onSignUp(user) {
+    let stateCopy = JSON.parse(JSON.stringify(this.state))
+    stateCopy.user = user
+    this.setState(stateCopy)
   }
   componentDidUpdate() {
     // localStore.save('todoList', this.state.todoList)
